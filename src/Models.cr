ce193f97eb6139495d5d1c09288d904c7f4b646c
Cassidy Dingenskirchen <admin@15318.de>
2020-06-23 16:07:30 +0200

    Whoops forgot to delete this

--- src/Models.cr
+++ src/Models.cr
@@ -1,40 +1,4 @@
 module Models
-  # Requests sent to Member bots (via unidirectional channel)
-  alias MemberRequest = {Command, Data}
-
-  # All the data that can be attached to an MemberRequest
-  alias Data = Nil |
-               String |
-               {Discord::Snowflake, String} |
-               {Discord::Snowflake, Discord::Snowflake, String} |
-               Discord::Snowflake |
-               {Discord::Snowflake, Discord::Snowflake}
-
-  # The different kinds of request to Member bots
-  # Crystal sadly seems to lack tagged unions, the comments document what types
-  # Member#run can expect to receive alongside this command.
-  # Be nice to Member#run.
-  enum Command
-    # Log in to Discord. Expects Nil.
-    Initialize
-    # Update bot presence to the tag of User. Expects User : Discord::Snowflake.
-    UpdateMemberPresence
-    # Post Content in ChannelId. Expects {Content : String, ChannelId : Discord::Snowflake}.
-    Post
-    # Edit MessageId in ChannelId to Content. Expects {ChannelId : Discord::Snowflake, MessageId : Discord::Snowflake, Content : String}.
-    Edit
-    # Update username to Username. Expects Username : String.
-    UpdateUsername
-    # Update avatar image to the image pointed at by Url. Expects Url : String.
-    UpdateAvatar
-    # Update nick in GuildID to Val. Expects {GuildID : Discord::Snowflake, Val : String}
-    UpdateNick
-    # Delete own message MessageID in channel ChannelID. Expects {MessageID : Discord::Snowflake, ChannelID : Discord::Snowflake}.
-    Delete
-    # Shut down the member bot. Expects Nil.
-    Shutdown
-  end
-
   # Helper class to safely convert DB strings to Crystal strings without really
   # obnoxious edge cases. Trust me, this is good.
   class DBString
